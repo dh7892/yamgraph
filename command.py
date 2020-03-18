d90a3e5821c3f30a6b9929baafa59088cde8f53b
David Hills <dave.a.hills@gmail.com>
2020-03-18 21:49:04 +0000

    Fixed up output code

--- command.py
+++ command.py
@@ -14,7 +14,6 @@ def process(output_file):
     """
     print(f"Outputting to file: {output_file}")
     drawing = OutputDriver(output_file)
-    drawing.draw_background("moo", 0, 0, 180, 90)
     drawing.output()
 
 
--- output_driver.py
+++ output_driver.py
@@ -1,17 +1,25 @@
 """
-This module contains the the base class for an output driver.
-The idea is that we can specialise this class to provide output in different image
-formats
+This module contains the code the actually produces graphics output.
+
+The idea is that other parts of the code can make calls in here to draw
+simple shapes and this will handle the job of actually converting that
+to output in whatever format we've requested
 """
+from colour import Colour
+
+# Define some useful colour constants (we may move these later)
+BACKGROUND_COLOUR = Colour("#FE8F80")
 
 
 class OutputDriver:
     """
-    This class can be used to do all of the drawing
+    This class can be used to do draw some simple shapes
+    Each drawing operation will add the shape to a buffer.
+    This will only be written to the file when output() is
+    called. The idea of this is so that we don't 
+    leave a file open for a while as we're putting in each
+    shape. 
     """
-
-    # Define some useful colours that we can use in any driver.
-    background_colour = (x/255 for x in (254, 143, 128))
 
     def __init__(self, filename, width=200, height=150):
         # Remember the canvas size in case we want to scale
@@ -19,25 +27,24 @@ class OutputDriver:
         self.width = width
         self.height = height
 
-    def draw_background(self, name: str, x: int, y: int, width: int, height: int) -> None:
-        """
-        Draw the standard background for the diagram, this is an orange box with the name of the cluster in it
-        :param name: The name to put inside the box
-        :param x: The x-coord of the box
-        :param y: The y-coord of the box
-        :param width: The width of the box (pixels)
-        :param height: The height of the box (pixels)
-        :return: None
+    def draw_box(self, x: int, y: int, width: int, height: int, colour: Colour):
+        """Draw a box
+        
+        Args:
+            x (int): The x position of the left side of the box
+            y (int): The y position of the box (top or bottom TBC)
+            width (int): The width of the box 
+            height (int): The height of the box
+            colour (Colour): The colour of the background of the box
+        Return: None
         """
         pass
+
 
     def output(self) -> None:
         """
         Write out the diagram to the filename specified.
         NOTE: all "drawing" should already have been done by calling other methods first, to build up an image. This
         is the final step to output the image to a file.
-        :param filename: The name of the file to write to
-        :param filetype: The type of file to write (e.g. "svg")
-        :return: None
         """
         print(f"This is where we will write a file for you!")
