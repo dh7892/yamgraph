8a2bd525a6bd35406ca433d6afee0a183bc4d856
David Hills <dave.a.hills@gmail.com>
2020-03-20 11:49:33 +0000

    Added test for draw deployments

--- tests/test_yaml_parser.py
+++ tests/test_yaml_parser.py
@@ -4,6 +4,7 @@ Unit tests for yaml_parser
 
 import pytest
 import tempfile
+from unittest import mock
 
 import yaml_parser
 
@@ -34,3 +35,14 @@ def test_empty_data(empty_data):
     """Test that we gracefully get an empty list of data if we feed an empty
     string to the function
     """
+
+
+@mock.patch("output_driver.OutputDriver")
+def test_draw_deployments(MockOutputDriver, simple_yml_data):
+    """Test that we can draw a simple deployment
+    """
+    data = yaml_parser.read_yaml(simple_yml_data)
+    yaml_parser.draw_deployments(data)
+
+    # Check that we called "draw_box" once
+    assert MockOutputDriver.draw_box.called
